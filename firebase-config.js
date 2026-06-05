2d8c29467fa3294ce8090167785f71de7d28474b
rg-ru <188490540+rg-ru@users.noreply.github.com>
2026-06-05 02:38:36 +0200

    Add Firebase web app config

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 window.ORTHODOX_COMPANION_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  appId: "",
-  messagingSenderId: "",
-  storageBucket: "",
-  measurementId: "",
+  apiKey: "AIzaSyD06AqR9no53vQQja-oQl7yPocEOBK1sAc",
+  authDomain: "orthodox-companion-65087.firebaseapp.com",
+  projectId: "orthodox-companion-65087",
+  appId: "1:583750549339:web:df22e48aca9d4efb03247e",
+  messagingSenderId: "583750549339",
+  storageBucket: "orthodox-companion-65087.firebasestorage.app",
+  measurementId: "G-9MMCH2PB6S",
 };
 
 window.ORTHODOX_COMPANION_ADMIN_EMAILS = ["admin@orthodox-companion.app"];
